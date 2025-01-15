36b0704f1be4bba51aa28818791956ef28202a24
Michael Pulusan <mjpulusan@yahoo.com>
2025-01-15 19:35:46 +0000

    Completed prep-javascript-operators-and-expressions exercise

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+//multiplication
+var number1 = 2;
+var number2 = 14;
+const product = number1 * number2;
+console.log('value of the product is = ', product);
+console.log('typeof product:', typeof product);
+
+//subtraction
+var charge = 10.5;
+var payment = 5;
+const amountRemaining = charge - payment;
+console.log('value of amountRemaining = ', amountRemaining);
+console.log('typeof amountRemaining:', typeof amountRemaining);
+
+//average grade
+var test = 75;
+var assignments = 90;
+var final = 80;
+let grade = (test + assignments + final) / 3;
+console.log('value of grade = ', grade);
+console.log('typeof grade:', typeof grade);
+
+//fullName
+const firstName = 'Michael John';
+const lastName = 'Pulusan';
+const fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+//Acidic value
+var pH = 4;
+const isAcidic = pH < 7; //true
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+//number of Soldiers
+var numberOfSoldiers = 5;
+const isSparta = numberOfSoldiers === 300; //false
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+//nickName
+let nickname = fullName;
+nickname += ' is the GOAT';
+console.log('value of nickname:', nickname);
+console.log('typeof nickname:', typeof nickname);
